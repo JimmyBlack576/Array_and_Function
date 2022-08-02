4d89e9b8b191336d4363defe3628d32ca31a6f42
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-08-02 11:05:21 +0300

    Переделал 3е задание

--- Array_and_Function/Array_and_Function.cpp
+++ Array_and_Function/Array_and_Function.cpp
@@ -5,12 +5,15 @@ using namespace std;
 long nDays(long y, long m, long d);
 //Функция расчёта среднего арифметического 
 void average(int* arr, int s);
-//Функция перевода 2 сс в десятичнную
-int convert(long long n);
+//Функция подсчёта отрицательных, положительных и нулевых элементов массива
+void value_count(int* arr3);
+
 
 
 int main()
 {
+    int arr3[10] = { -1,3,-5,0,4,7,4,-4,0,9 };
+    int* ptr = arr3;
     int arr[10] = { 12,3,21,4,2,514,35,32,12,75 };
     //Task 1___________________________________________
     long y, m, d;
@@ -21,38 +24,39 @@ int main()
     cin >> d >> m >> y;
     long nd2 = nDays(y, m, d);
 
-    cout <<"Amount of days: "<< nd2 - nd1<<endl;
+    cout <<"Amount of days: "<< nd2 - nd1<<endl<<endl;
+    system("pause");
     //_________________________________________________
     //Task 2
     average(arr, 0);
+    system("pause");
     //_________________________________________________
     //Task 3
-    long long n;
-    cout << "Enter a binary number: ";
-    cin >> n;
-    cout << n << " in binary = " << convert(n) << " in decimal";
+    value_count(arr3);
     return 0;
 }
 
 long nDays(long y, long m, long d) {
     if (m <= 2) m += 12, y -= 1;
     return (y * 146097) / 400 + (m * 153 + 8) / 5 + d;
+    
 }
 
 void average(int* arr, int s) {
     for (int i = 0; i < 10; i++) {
         s += arr[i];
     }
-    cout << "Average: " << s / 10 << endl;
+    cout << "Average: " << s / 10 << endl<<endl;
 }
 
-int convert(long long n) {
-    int dec = 0, i = 0, temp;
-    while (n != 0) {
-        temp = n % 10;
-        n /= 10;
-        dec += temp * pow(2, i);
-        ++i;
+void value_count(int* arr3) {
+    int cp = 0, cn = 0, cnull = 0;
+    for (int i = 0; i < 10; i++) {
+        if (*(arr3 + i) < 0) cn++;
+        else if (*(arr3 + i) == 0) cnull++;
+        else if (*(arr3 + i) > 0) cp++;
     }
-    return dec;
+    cout << "Negative- " << cn << endl;
+    cout << "Positive- " << cp << endl;
+    cout << "Null- " << cnull << endl;
 }
